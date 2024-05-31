9f339df12a36717a17f86eea50a313bf1243d52b
Nematov Ro'zimuhammad <jeki2004114r@gmail.com>
2024-05-31 22:55:47 +0500

    main

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ function darkTheme(){
     element.classList.toggle("dark-mode");
 
 }
+
+function btn(){
+    let element= document.querySelector(".nav-item")
+    element.classList.toggle("btn")
+}
+
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    header.classList.add("header__shrink");
+  } else {
+    header.classList.remove("header__shrink");
+  }
+});
